38d10704c71c672bbb6ab2fbc848723836a114f0
Aditya Agarwal <agarwaladitya512@gmail.com>
2025-11-16 12:21:32 +0530

    done with the user work

--- dist/routes/user.js
+++ dist/routes/user.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { deleteUser, getAllUsers, getUsers, newUser } from '../controllers/user.js';
+import { adminOnly } from '../middlewares/auth.js';
 const app = express.Router();
 //route -/api/v1/user/new
 app.post("/new", newUser);
 //route- /api/vi/user/all
-app.get("/all", getAllUsers);
+app.get("/all", adminOnly, getAllUsers);
+app.route("/:id").get(getUsers).delete(adminOnly, deleteUser);
 //route- /api/vi/user/dynamic id
-app.get("/:id", getUsers);
+//app.get("/:id",getUsers);
 //route for delete user- /api/vi/user/dynamic id
-app.get("/:id", deleteUser);
+//app.get("/:id",deleteUser);
 export default app;
